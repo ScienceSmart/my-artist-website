359b629fe3113029003365423ecb3c869f5075eb
Erkin Khoroshi <calculus.antiderivative@gmail.com>
2025-05-30 12:24:53 -0400

    Altered the layout of the Login Page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function Login() {
   return (
     <>
-     {/* Header Section */}
-     <header className="bg-headerSection shadow-md flex flex-col justify-center items-center">
-        <div className="container flex flex-col items-center py-6 px-4">
+    {/* Header Section */}
+      <header className="bg-headerSection shadow-md flex flex-col justify-center items-center border-b-4 border-t-4 border-gray-600">
+        <div className="container flex flex-col items-center py-6 px-4 ">
           <div className="text-center text-black">
             <h1 className="text-3xl font-bold">Erkin Khoroshi Eisalou</h1>
             <p className="text-lg">Storyboard Artist/Developer</p>
@@ -13,9 +13,9 @@ export default function Login() {
         </div>
 
         {/* Login In Button */}
-        <div className="absolute right-20">
+        <div className="absolute right-20 shadow-lg">
           <Link href="/login">
-            <button className="bg-white text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition">
+            <button className="bg-white border-2 border-gray-600 text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition">
               Login
             </button>
           </Link>
@@ -23,45 +23,41 @@ export default function Login() {
       </header>
 
       {/* Navigation Bar Section */}
-      <nav className="bg-navbar text-white shadow">
+      <nav className="bg-navbar text-white shadow border-b-4 border-gray-600">
         <div className="container mx-auto flex justify-center space-x-6 py-3">
           <Link href="/">
-            <button className="bg-white text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition"> 
+            <button className="bg-white border-2 border-gray-600 text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition shadow-lg"> 
                 Home
             </button>
           </Link>
           <Link href="/storyboards">
-            <button className="bg-white text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition"> 
+            <button className="bg-white border-2 border-gray-600 text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition shadow-lg"> 
                 Storyboards
             </button>
           </Link>
           <Link href="/animations">
-            <button className="bg-white text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition"> 
+            <button className="bg-white border-2 border-gray-600 text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition shadow-lg"> 
                 Animations
             </button>
           </Link>
           <Link href="/coding-projects">
-            <button className="bg-white text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition"> 
+            <button className="bg-white border-2 border-gray-600 text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition shadow-lg"> 
                 Coding Projects
             </button>
           </Link>
           <Link href="/illustrations">
-            <button className="bg-white text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition"> 
+            <button className="bg-white border-2 border-gray-600 text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition shadow-lg"> 
                 Illustrations
             </button>
           </Link>
-          <Link href="/paintings">
-            <button className="bg-white text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition"> 
-                Paintings
-            </button>
-          </Link>
           <Link href="/resume">
-            <button className="bg-white text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition"> 
+            <button className="bg-white border-2 border-gray-600 text-black font-semibold px-4 py-2 rounded hover:bg-gray-100 transition shadow-lg"> 
                 Resume
             </button>
           </Link>
         </div>
       </nav>
+
 
        {/* Main Body */}
       <div className="flex h-screen">
@@ -71,9 +67,9 @@ export default function Login() {
         </aside>
 
         {/* Main Content */}
-        <main className="flex-1 flex-col px-10 py-4 justify-left bg-main text-black">
+        <main className="container flex-1 flex-col px-10 py-4 justify-left bg-main text-black ">
 
-          <div className="max-w-md mx-auto mt-8 p-6">
+          <div className="flex-row bg-white p-5 my-5 border-4 border-gray-600 rounded-md shadow-lg">
             <h1 className="text-3xl font-bold text-center mb-4 mt-25 py-5">Login</h1>
             {/* Form */}
             <form>
